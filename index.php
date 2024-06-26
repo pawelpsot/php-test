ded68e6d1a8f5f6e0b0d9194baac5ba08b658b75
pawelpsot <kajtek15.pk@gmail.com>
2024-06-26 19:42:31 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!-- index.php -->
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Podręcznik ezdrp</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div id="spacer"></div> 
+<div id="header">
+    <h1><a href="index.php">Podręcznik ezdrp szpital nr. 4</a></h1>
+</div>
+
+<?php include 'navigation.php'; ?>
+
+<script src="js/zdjecia.js"></script>
+<script src="js/nawi.js"></script>
+<script src="https://unpkg.com/scrollreveal"></script>
+
+</body>
+</html>
