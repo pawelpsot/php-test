a18fd0c2a89c25cbfecfe9edf544e0add62d4d0d
pawelpsot <kajtek15.pk@gmail.com>
2024-06-26 22:36:04 +0200

    Delete nawi.js

--- nawi.js
+++
@@ -1,9 +0,0 @@
-<script>
-    document.addEventListener("DOMContentLoaded", function() {
-        fetch('navigation.html')
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById('navigation-container').innerHTML = data;
-            });
-    });
-</script>
